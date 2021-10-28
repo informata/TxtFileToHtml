7d82b7d14241e21f6a4a69323aa4768a158bb431
informata <informata@informata.es>
2021-10-28 19:48:49 +0200

    Create sqlite-in-replit.sh

---
+++ sqlite-in-replit/sqlite-in-replit.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+# Install SQLite3 with PHP on Repl.it
+# 
+# Create a new Repl.it as a PHP Web Server and run this script.
+# wget -O - https://raw.githubusercontent.com/informata/php-fragments/master/sqlite-in-replit.sh | bash
+
+#Download, extract and cleanup required missing PHP modules (MySQL for PDO and SQLite for SQLITE!)
+wget http://archive.ubuntu.com/ubuntu/pool/main/p/php7.2/php7.2-sqlite3_7.2.24-0ubuntu0.18.04.10_amd64.deb
+for Module in $( ls php*.deb ); do dpkg -x $Module .; done
+mkdir ~/$REPL_SLUG/php
+cp usr/lib/php/*/* ~/$REPL_SLUG/php
+for Module in $( ls ~/$REPL_SLUG/php/*.so ); do echo "extension=$Module" >> ~/$REPL_SLUG/php/php.ini; done
+rm -rf ./etc/ 
+rm -rf ./usr/
+rm php7.2-sqlite3_7.2.24-0ubuntu0.18.04.10_amd64.deb
+
+#Setup the repl to start PHP with the correct php.ini that includes our modules
+echo 'run = "php -c ~/$REPL_SLUG/php/php.ini -S 0.0.0.0:8000"' >> .replit
+
+echo "Done!"
